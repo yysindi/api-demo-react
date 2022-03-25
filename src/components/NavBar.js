b2ff18ddc4c4a5ab38e94bdf699a4efd4f9ff7ff
Yusuf Sindi <yysindi@ymail.com>
2022-03-25 14:59:40 +0000

    changed font and added toggle button to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.scss';
 import logo from '../assets/logo.svg';
+import DarkModeToggle from 'react-dark-mode-toggle';
+
 const NavBar = () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
   return (
     <div class='navbar'>
       <Link to='/'>
@@ -11,6 +14,15 @@ const NavBar = () => {
       <Link to='/card'>Cards</Link>
       <Link to='/transactions'>Transactions</Link>
       <Link to='/fundingsource'>Funding</Link>
+      <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={80} />
+      <a href='https://lithic.com' target='_blank' rel='noreferrer'>
+        {' '}
+        Learn More{' '}
+      </a>
+      <a href='https://docs.lithic.com' target='_blank' rel='noreferrer'>
+        {' '}
+        API Docs{' '}
+      </a>
     </div>
   );
 };
